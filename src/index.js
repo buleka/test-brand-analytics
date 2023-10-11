5f4f02e9b2e6e08a0b457d74c28ec5039b171844
olga <buleka>
2023-10-11 10:24:30 +0300

    code refactor

--- src/index.js
+++ src/index.js
@@ -3,24 +3,41 @@ import "./sass/style.scss";
 const breakpointMobile = 767;
 const ticketsTransform = Array.from(document.getElementsByClassName('ticket--transform'));
 const container = document.getElementsByClassName('section')[0];
-const heightContainer = container.clientHeight;
+let heightContainer = container.clientHeight;
 
-const isNotMobile = () => window.innerWidth >= breakpointMobile;
+const isMobile = () => window.innerWidth < breakpointMobile;
 
-window.addEventListener('scroll', function () {
-    if (!isNotMobile()) {
-        return;
-    }
-    let positionContainer = container.getBoundingClientRect().y;
+let lastScrollTop = 0;
+
+function tick() {
+    let positionTop = container.getBoundingClientRect().y;
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (positionContainer < scrollTop && scrollTop < positionContainer + heightContainer) {
-        ticketsTransform.forEach((item, index)=>{
-            const co = index * 3 - 7;
+    if (scrollTop !== lastScrollTop &&
+        positionTop < scrollTop &&
+        scrollTop < positionTop + heightContainer) {
+
+        ticketsTransform.forEach((item, index) => {
+            let magicNumber = isMobile() ? (index * 2 - 4) : (index * 3 - 7);
             let styles = window.getComputedStyle(item).getPropertyValue('transform');
             let values = styles.split('(')[1].split(')')[0].split(', ');
             const scaleValue = values[0];
-            item.style = "transform: translate3d(0 ," + (scrollTop - positionContainer) / co + "px, 0) scale(" + scaleValue + ")";
-        })
+
+            item.style.transform = `translate3d(0, ${(scrollTop - positionTop)/magicNumber}px, 0) scale(${scaleValue})`;
+        });
+
+        lastScrollTop = scrollTop;
     }
+
+    requestAnimationFrame(tick);
+}
+
+tick();
+
+window.addEventListener('scroll', function () {
+    heightContainer = container.clientHeight;
+});
+
+window.addEventListener('resize', function () {
+    heightContainer = container.clientHeight;
 });
